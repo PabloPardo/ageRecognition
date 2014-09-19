fb564114beb1dec23f2d692ac6c10cf400743cf7
Pablo Pardo Garcia <ppardoga7@gmail.com>
2014-09-19 15:08:13 +0200

    Change to mysql db

--- ageRecognition/ageRecognition/settings/base.py
+++ ageRecognition/ageRecognition/settings/base.py
@@ -406,8 +406,8 @@ class DevBase(Base):
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': os.path.join(Base.PROJECT_DIR, 'dev_db.sqlite'), # Or path to database file if using sqlite3.
+            'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': os.path.join(Base.PROJECT_DIR, 'dev_db.mysql'), # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': '',
             'PASSWORD': '',
