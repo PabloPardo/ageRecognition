54443d63937d738694bd82b2e1029c3462466c17
Pablo Pardo Garcia <ppardoga7@gmail.com>
2014-10-15 01:57:08 +0200

    Fixed some realoading problems

--- ageRecognition/apps/canvas/static/js/dropzone.js
+++ ageRecognition/apps/canvas/static/js/dropzone.js
@@ -438,7 +438,7 @@ require.register("dropzone/lib/dropzone.js", function (exports, module) {
             node.innerHTML = this.filesize(file.size);
           }
           if (this.options.addRemoveLinks) {
-            file._removeLink = Dropzone.createElement("<a class=\"dz-remove\" href=\"javascript:undefined;\" data-dz-remove>" + this.options.dictRemoveFile + "</a>");
+            file._removeLink = Dropzone.createElement("<a class=\"dz-remove\" href=\"javascript:undefined;\" onclick='location.reload();' data-dz-remove>" + this.options.dictRemoveFile + "</a>");
             file.previewElement.appendChild(file._removeLink);
           }
           removeFileEvent = (function(_this) {
