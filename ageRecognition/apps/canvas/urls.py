cb2703250932d4cfb27e52d0890c5e83a0b0c99f
DJango user for facebook application <django@sunai.uoc.edu>
2014-11-08 17:09:29 +0100

    URL chaned

--- ageRecognition/apps/canvas/urls.py
+++ ageRecognition/apps/canvas/urls.py
@@ -13,5 +13,5 @@ urlpatterns = patterns('apps.canvas.views',
     url(r'^privacy/$', 'privacy', name='privacy'),
     url(r'^help/$', 'help', name='help'),
     url(r'^prizes/$', 'prizes', name='prizes'),
-    url(r'^test/$', 'test', name='test')
+    url(r'^test/$', 'test', name='test'),
 )
